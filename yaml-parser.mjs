5366de50990bfc919e98f80f3f2dd48e991eb7c6
Lucas Maspero <masperolucas@gmail.com>
2020-09-10 11:29:12 -0300

    Added string format

--- yaml-parser.mjs
+++ yaml-parser.mjs
@@ -1,10 +1,18 @@
 import SwaggerParser from "@apidevtools/swagger-parser";
+import format from "string-template";
 
 SwaggerParser.validate("apilucas.yaml", (err, api) => {
   if (err) {
     console.error(err);
   } else {
-    var lala = "##teamcity[setParameter name='ApiVersion' value='" + api.info.version + "']";
-    console.log(lala);
+    addTeamCityVariable("ApiVersion", api.info.version);
   }
 });
+
+function addTeamCityVariable(name, value) {
+  const teamCityVariableTemplate = "##teamcity[setParameter name='{0}' value='{1}']";
+  var teamCityVariable = format(teamCityVariableTemplate, name, value);
+  
+  // Redirect message to output to set up TeamCity variable
+  console.log(teamCityVariable)
+}
